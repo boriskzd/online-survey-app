efa65d20e0935bfb7430264ddd3b6274c8b03fed
boriskzd <boris.knezevic1@yahoo.com>
2021-11-13 16:47:40 +0100

    add navbar
    add navbar component with survey and success buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import SuccessPage from './views/SuccesPage';
 import SurveyPage from './views/SurveyPage';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<SurveyPage />} />
-        <Route path='success' element={<SuccessPage />} />
-      </Routes>
-      <Counter />
+      <Container maxWidth='sm'>
+        <Routes>
+          <Route path='/' element={<SurveyPage />} />
+          <Route path='success' element={<SuccessPage />} />
+        </Routes>
+
+        <Counter />
+      </Container>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   return (
-    <nav>
-      Navbar
-      <Link to='/'>Home</Link> <Link to='/success'>Success</Link>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            Survey App
+          </Typography>
+          <RouterLink to='/'>
+            <Button variant='contained' color='info' sx={{ marginRight: 1 }}>
+              Survey
+            </Button>
+          </RouterLink>
+          <RouterLink to='/success'>
+            <Button variant='contained' color='info'>
+              Success
+            </Button>
+          </RouterLink>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
